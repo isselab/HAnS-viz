bbe9481c4a18b05053007d5891cdfaa5300976af
Kuzzi04 <95016273+Kuzzi04@users.noreply.github.com>
2024-01-02 13:13:48 +0100

    add unversioned

---
+++ src/main/java/com/github/johmara/hansviz/browser/BrowserWindowFactory.java
@@ -0,0 +1,17 @@
+package com.github.johmara.hansviz.browser;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class BrowserWindowFactory implements ToolWindowFactory {
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        BrowserViewerWindow browserViewerWindow = project.getService(BrowserViewerService.class).browserViewerWindow;
+        JComponent component = toolWindow.getComponent();
+        component.getParent().add(browserViewerWindow.content(),0);
+    }
+}
---
+++ src/main/java/com/github/johmara/hansviz/browser/ClosedConnection.java
@@ -0,0 +1,30 @@
+// Copyright 2021 Kenny Bang, Johan Berg, Seif Bourogaa, Lucas Frövik, Alexander Grönberg, Sara Persson
+package com.github.johmara.hansviz.browser;
+
+import org.cef.callback.CefCallback;
+import org.cef.misc.IntRef;
+import org.cef.misc.StringRef;
+import org.cef.network.CefResponse;
+
+/**
+ * A class representing a closed connection to a resource
+ */
+class ClosedConnection implements ResourceHandlerState {
+
+    /**
+     * Always responds status: 404 Not Found.
+     */
+    @Override
+    public void getResponseHeaders(CefResponse cefResponse, IntRef responseLength, StringRef redirectUrl) {
+        cefResponse.setStatus(404);
+    }
+
+    /**
+     * A closed connection can't read responses and thus always return false
+     * @return False
+     */
+    @Override
+    public boolean readResponse(byte[] dataOut, int designedBytesToRead, IntRef bytesRead, CefCallback callback) {
+        return false;
+    }
+}
