a4b16418274e829e4d9f15b47b5da327b81efb16
HerrKermet <david.stechow1113@gmail.com>
2024-01-21 15:07:20 +0100

    Added Contributor-Info

--- src/main/resources/webcontent/js/main.js
+++ src/main/resources/webcontent/js/main.js
@@ -1,3 +1,19 @@
+/*
+Copyright 2024 David Stechow & Philipp Kusmierz
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 const chartDom = document.getElementById('main');
 
 const body = document.querySelector(".box");
@@ -502,7 +518,6 @@ function openScattering(){
             {
                 name: '',
                 type: 'graph',
-                /*layout: state.showTanglingAsNormalGraph ? "force" : "circular",*/
                 layout: "force",
                 circular: {
                     rotateLabel: true
@@ -518,7 +533,6 @@ function openScattering(){
                     if(entry.lines === feature.lines)
                         entry["symbolSize"] = size;
                     else
-                        /*entry["symbolSize"] = Math.min((entry.lines / feature.lines) * size * 2, size);*/
                         entry["symbolSize"] = [0.8 * size, size];
                     return entry;
                 }),
